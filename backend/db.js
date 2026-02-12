49946ca7a53ae68354be5d047312d9bbbc042380
nitinmeruva2005 <147080458+nitinmeruva2005@users.noreply.github.com>
2026-02-12 13:57:05 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -9,11 +9,11 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.DB_HOST,
+  port: 3306, // Railway default
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -21,6 +21,7 @@ const pool = mysql.createPool({
     rejectUnauthorized: false
   }
 });
+
 
 
 async function query(sql, params) {
